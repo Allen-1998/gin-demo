e9dac8a979631c0a16d3312c8bef7aae79161c1a
allen <272970666@qq.com>
2023-03-21 18:14:43 +0800

    feat: 基本路由

--- main.go
+++ main.go
@@ -1,13 +1,56 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+
+	// 基本路由
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "hello World!")
+	})
+
+	// 路由分组
+	user := r.Group("/user")
+	{
+		user.GET("/", func(c *gin.Context) {
+			name := c.Query("name")
+			c.JSON(http.StatusOK, gin.H{
+				"name":    name,
+				"message": "success",
+			})
+		})
+		user.POST("/login", func(c *gin.Context) {
+			name := c.PostForm("name")
+			if name == "" {
+				c.JSON(http.StatusBadRequest, gin.H{
+					"message": "name is not defined",
+				})
+				return
+			}
+			password := c.PostForm("password")
+			if password == "" {
+				c.JSON(http.StatusBadRequest, gin.H{
+					"message": "password is not defined",
+				})
+				return
+			}
+			c.JSON(http.StatusOK, gin.H{
+				"name":     name,
+				"password": password,
+				"message":  "success",
+			})
+		})
+	}
+	return r
+}
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // 监听并在 0.0.0.0:8080 上启动服务
+	r := setupRouter()
+	// 监听端口，默认在8080
+	r.Run()
 }
