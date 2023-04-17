832564bc050516414473709b0736d90f4d4e62b0
allen <272970666@qq.com>
2023-04-18 01:05:54 +0800

    feat: 404

--- routers/index/router.go
+++ routers/index/router.go
@@ -1,10 +1,18 @@
 package index
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func Router(r *gin.Engine) {
 	r.LoadHTMLGlob("templates/*")
 	r.Static("/static","./static")
+
+	r.NoRoute(func(c *gin.Context) {
+		c.HTML(http.StatusNotFound, "404.html", nil)
+	})
 	index := r.Group("/")
 	indexIn(index)
 }
